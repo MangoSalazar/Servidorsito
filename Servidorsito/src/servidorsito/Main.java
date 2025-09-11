a14e953cccc9572d20f0bafd2ac9b45389ee9e0e
mango <mango@Mango-PC>
2025-09-11 00:03:17 -0700

    Se añadio el inicio de sesion

--- Servidorsito/src/servidorsito/Main.java
+++ Servidorsito/src/servidorsito/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -36,51 +37,69 @@ public class Main {
             escritor = new PrintWriter(cliente.getOutputStream(),true);
             BufferedReader lectorSocket = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
             
-            File archivo;
+            File archivo = null;
             try {
                 archivo = new File("C:\\Users\\mango\\Documents\\Servidorsito\\Servidorsito\\src\\servidorsito\\Registros.txt");
-                FileReader lectorArchivo = new FileReader(archivo);
-                BufferedReader lectorMejorado = new BufferedReader(lectorArchivo);
-            
+                FileReader lRegistros = new FileReader(archivo);
+                BufferedReader lectorRegistros = new BufferedReader(lRegistros);
             } catch (FileNotFoundException e) {
                 System.out.println("Hubo un problema con los registros.");
                 e.printStackTrace();
             }
-
-        
+            
+            String nombre;
+            String contraseña;
             escritor.println("¿Tienes una sesion? (si/no)");
-            String entrada;
-            int numerito = (int) (Math.random() * 10);
-            System.out.println("Numero secreto: " + numerito); 
-            int intentos = 0;
-            boolean adivino = false;
-
-            while ((entrada = lectorSocket.readLine()) != null && intentos < 3) {
+            String entrada = lectorSocket.readLine();
+            
+            if (entrada.equals("no") || (entrada.toUpperCase()).equals("NO")) {
+                escritor.println("Dame tu nombre de usuario");
+                nombre = lectorSocket.readLine();
+                escritor.println("Dame la contraseña");
+                contraseña = lectorSocket.readLine();
                 
-                int numero;
-                try {
-                    numero = Integer.parseInt(entrada);
-                } catch (NumberFormatException e) {
-                    escritor.println("Eso no es un número válido.");
-                    continue;
+                try (FileWriter fescritor = new FileWriter(archivo)) {
+                    fescritor.write(nombre + "_" + contraseña + "\n");
+                    System.out.println("Usuario registrado correctamente.");
+                } catch (IOException e) {
+                    System.out.println("Error al guardar el usuario.");
+                    e.printStackTrace();
                 }
-
-                if (numero > numerito) {
-                    escritor.println("Te pasaste");
-                    intentos++;
-                } else if (numero < numerito) {
-                    escritor.println("Te faltó");
-                    intentos++;
-                } else {
-                    escritor.println("¡Acertaste pa! ¿Quieres jugar de nuevo?");
-                    adivino = true;
-                    break;
-                }
-            }
-            if (!adivino) {
-                escritor.println("El número era: " + numerito);
             }
             
+            if (entrada.equals("si") || (entrada.toUpperCase()).equals("SI")) {                
+                int numerito = (int) (Math.random() * 10);
+                System.out.println("Numero secreto: " + numerito); 
+                int intentos = 0;
+                boolean adivino = false;
+
+                while ((entrada = lectorSocket.readLine()) != null && intentos < 3) {
+
+                    int numero;
+                    try {
+                        numero = Integer.parseInt(entrada);
+                    } catch (NumberFormatException e) {
+                        escritor.println("Eso no es un número válido.");
+                        continue;
+                    }
+
+                    if (numero > numerito) {
+                        escritor.println("Te pasaste");
+                        intentos++;
+                    } else if (numero < numerito) {
+                        escritor.println("Te faltó");
+                        intentos++;
+                    } else {
+                        escritor.println("¡Acertaste pa! ¿Quieres jugar de nuevo?");
+                        adivino = true;
+                        break;
+                    }
+                }
+                if (!adivino) {
+                    escritor.println("El número era: " + numerito);
+                }
+            }
+
         } catch (Exception e) {
             System.out.println("Hubo un problema con el lector");
             System.exit(1);
